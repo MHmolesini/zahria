1933648c7bf7f9333a8712bf7f0f7acfdf3a8ab6
MHmolesini <Mhmolesini@gmail.com>
2023-02-18 21:33:20 -0300

    Se creo el archivo main y App.jsx

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import {useState} from 'react'
+
+function App(){
+    return(
+        <header>
+            <nav>
+                <ul>
+                    <li>Tienda</li>
+                    <li>Contactanos</li>
+                    <li>Sobre nosotros</li>
+                    <li>Preguntas frecuentes</li>
+                </ul>
+            </nav>
+        </header>
+    )
+}
+
+export default App
